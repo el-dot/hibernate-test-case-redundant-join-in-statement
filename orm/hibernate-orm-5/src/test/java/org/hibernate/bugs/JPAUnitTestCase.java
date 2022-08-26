4035b491da49b6bedfe9dd13bb83c100fdb9b91a
Krzysztof Bieroń <7493083+el-dot@users.noreply.github.com>
2022-08-26 19:11:05 +0200

    Add test case for redundant join and subquery in update statement

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,16 @@
 package org.hibernate.bugs;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
+import org.hibernate.resource.jdbc.spi.StatementInspector;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -17,7 +21,10 @@ public class JPAUnitTestCase {
 
 	@Before
 	public void init() {
-		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
+		entityManagerFactory = Persistence.createEntityManagerFactory(
+				"templatePU",
+				Collections.singletonMap("hibernate.session_factory.statement_inspector", StatementCollector.class)
+		);
 	}
 
 	@After
@@ -25,14 +32,51 @@ public class JPAUnitTestCase {
 		entityManagerFactory.close();
 	}
 
-	// Entities are auto-discovered, so just add them anywhere on class-path
-	// Add your tests, using standard JUnit.
 	@Test
 	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		// Do stuff...
-		entityManager.getTransaction().commit();
-		entityManager.close();
+		try {
+			entityManager.getTransaction().begin();
+			entityManager.createQuery("UPDATE Parent p SET p.someField = :value WHERE p.id = :id")
+					.setParameter("value", "someValue")
+					.setParameter("id", 1L)
+					.executeUpdate();
+		} finally {
+			entityManager.getTransaction().rollback();
+			entityManager.close();
+		}
+		String statement = StatementCollector.statements.stream()
+				.filter(it -> it.contains("someField"))
+				.findFirst().get();
+
+		assertFalse(statement.toLowerCase().contains("join"), () -> "Query should not contain `join`: `" + statement + "`");
+	}
+
+
+	public static class StatementCollector implements StatementInspector {
+		static List<String> statements = new ArrayList<>();
+
+		public String inspect(String s) {
+			statements.add(s);
+			return s;
+		}
+	}
+
+	@Entity(name = "Parent")
+	@Table
+	static class Parent {
+		@Id
+		Long id;
+		String someField;
+		@ManyToOne
+		@JoinTable(name = "parent_child")
+		Child child;
+	}
+
+	@Entity
+	@Table
+	static class Child {
+		@Id
+		Long id;
 	}
 }
--- orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-6/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,12 +1,17 @@
 package org.hibernate.bugs;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-
+import jakarta.persistence.*;
+import org.hibernate.resource.jdbc.spi.StatementInspector;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -17,7 +22,10 @@ public class JPAUnitTestCase {
 
 	@Before
 	public void init() {
-		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
+		entityManagerFactory = Persistence.createEntityManagerFactory(
+				"templatePU",
+				Map.of("hibernate.session_factory.statement_inspector", StatementCollector.class)
+		);
 	}
 
 	@After
@@ -25,14 +33,55 @@ public class JPAUnitTestCase {
 		entityManagerFactory.close();
 	}
 
-	// Entities are auto-discovered, so just add them anywhere on class-path
-	// Add your tests, using standard JUnit.
 	@Test
 	public void hhh123Test() throws Exception {
+		// given
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		// Do stuff...
-		entityManager.getTransaction().commit();
+
+		// when
+		entityManager.createQuery("UPDATE Parent p SET p.someField = :value WHERE p.id = :id")
+				.setParameter("value", "someValue")
+				.setParameter("id", 1L)
+				.executeUpdate();
+
+		// cleanup
+		entityManager.getTransaction().rollback();
 		entityManager.close();
+
+		// then
+		String statement = StatementCollector.statements.stream()
+				.filter(stmt -> stmt.startsWith("update"))
+				.filter(stmt -> stmt.contains("someField"))
+				.findFirst().get();
+		assertFalse(statement.toLowerCase().contains("select"), () -> "Query should not contain subquery: `" + statement + "`");
+	}
+
+
+	public static class StatementCollector implements StatementInspector {
+		static List<String> statements = new ArrayList<>();
+
+		public String inspect(String s) {
+			statements.add(s);
+			return s;
+		}
+	}
+
+	@Entity(name = "Parent")
+	@Table
+	static class Parent {
+		@Id
+		Long id;
+		String someField;
+		@ManyToOne
+		@JoinTable(name = "parent_child")
+		Child child;
+	}
+
+	@Entity
+	@Table
+	static class Child {
+		@Id
+		Long id;
 	}
 }
